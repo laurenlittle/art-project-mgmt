f3017a683fe826387ba263d9feccc54e7b4d05ea
Lauren Suero <littlelauren016@gmail.com>
2023-02-22 23:26:20 -0500

    Add postcss; Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./lib/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
